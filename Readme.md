9e204535d2356a5d2d563e499346d2df7baf6ba8
kangmingyi <jerry010201@163.com>
2025-03-18 18:11:00 +0800

    add readme

---
+++ Readme.md
@@ -0,0 +1,8 @@
+|序号|标题|leetcode地址|代码|
+|-|-|-|-|
+|0001|Two Sum|[Link](https://leetcode.com/problems/two-sum/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/0001_two_sum.go)|
+|0002|Add Two Numbers|[Link](https://leetcode.com/problems/add-two-numbers/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/0002_add_two_numbers.go)|
+|0003|Longest Substring Without Repeating Characters|[Link](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/0003_longest_substring_without_repeating_characters.go)|
+|0004|Median Of Two Sorted Arrays|[Link](https://leetcode.com/problems/median-of-two-sorted-arrays/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/0004_median_of_two_sorted_arrays.go)|
+|0005|Longest palindromic substring|[Link](https://leetcode.com/problems/longest-palindromic-substring/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/0005_longest_palindromic_substring.go)|
+|0006|Zigzag Conversion|[Link](https://leetcode.com/problems/zigzag-conversion/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/0006_zigzag_conversion.go)|
---
+++ generater.go
@@ -0,0 +1,83 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+func main() {
+	var id_str string = ""
+	var name string = ""
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	
+	var id_end bool = false
+	for i := 0; i < len(input); i++ {
+	  if !id_end {
+	    if input[i] == ' ' {
+	      id_end = true
+	      for i + 1 < len(input) && input[i + 1] == ' ' {
+	        i++
+	      }
+	    } else {
+    	  if input[i] < '0' || input[i] > '9' {
+    	    fmt.Print("Input error")
+    	    return
+    	  }
+    	  id_str += string(input[i])
+	    }
+	  } else {
+	    name += string(input[i])
+	  }
+	}
+	
+	if len(id_str) > 4 {
+	   fmt.Print("Input error")
+	   return
+	}
+	
+	for len(id_str) < 4 {
+	  id_str = "0" + id_str
+	}
+
+	var line string = "|"
+	line += id_str
+	line += "|"
+	line += name
+	line += "|[Link](https://leetcode.com/problems/"
+	para_nums := 0
+	var name_leetcode, name_github string = "", ""
+	for _, ch := range name {
+		if ch >= 'A' && ch <= 'Z' {
+			name_leetcode += string(ch - 'A' + 'a')
+			name_github += string(ch - 'A' + 'a')
+		} else if ch == ' ' {
+			name_leetcode += "-"
+			name_github += "_"
+		} else if ch == '(' {
+			para_nums++
+		} else if ch == ')' {
+			para_nums--
+		} else {
+			name_leetcode += string(ch)
+			name_github += string(ch)
+		}
+		if para_nums < 0 {
+			fmt.Print("Input error")
+			return
+		}
+	}
+	if para_nums != 0 {
+		fmt.Print("Input error")
+		return
+	}
+	line += name_leetcode
+	line += "/description/)|[Code](https://github.com/jerrykcode/leetcode-go/blob/main/Problems/"
+	line += id_str
+	line += "_"
+	line += name_github
+	line += ".go)|"
+	fmt.Print(line)
+}
